d301918a0118498edcf631deb3670217b9c7b1db
Bigyan <bigyanbhatta1193@gmail.com>
2024-06-09 16:05:33 +0545

    update in the code

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 from chatsql import ChatWithSql
 
 app = Flask(__name__)
 obj = ChatWithSql("root","6e6b697565","localhost","bigyan_database")
 
-@app.route('/send_message', methods = ['GET'])
-def send_message():
-    message = obj.message('How many columns are there in the table?')
-    return jsonify({'message': message})
+app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/send_message', methods=['GET','POST'])
+def send_message():
+    if request.method == 'POST':
+        user_query = request.form.get('query')
+        
+        if not user_query:
+            return jsonify({'error': 'No query provided'}), 400
+        
+        message = obj.message(user_query)
+        return jsonify({'message': message})
+    
+    return render_template('index.html')
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(debug=True)
